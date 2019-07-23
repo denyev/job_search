6a40e89364a079634398ee0561984522e506fa27
denyev <denevt87@gmail.com>
2019-07-23 20:53:48 +0300

    Changes a menu.

--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -38,21 +38,8 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/admin/default/index']],
-            ['label' => 'Vacancies', 'url' => ['/admin/vacancies/index']],
-            ['label' => 'Categories', 'url' => ['/admin/category/index']],
-            Yii::$app->user->isGuest ? (
-            ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            ['label' => 'Каталог', 'url' => ['/site/index']],
+            ['label' => 'Редактировать вакансии', 'url' => ['/admin/vacancies/index']],
         ],
     ]);
     NavBar::end();
