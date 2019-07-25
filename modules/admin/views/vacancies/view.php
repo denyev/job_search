1acd9d07352345053568e87c348d2c4dff75f662
denyev <denevt87@gmail.com>
2019-07-25 09:40:21 +0300

    Changes class names.

--- modules/admin/views/vacancies/view.php
+++ modules/admin/views/vacancies/view.php
@@ -16,12 +16,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Set image'), ['set-image', 'id' => $model->id], [
-                'class' => 'btn btn-warning'
+        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], [
+                'class' => 'btn btn-outline-primary'
         ]) ?>
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
+            'class' => 'btn btn-outline-danger',
             'data' => [
                 'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
                 'method' => 'post',
