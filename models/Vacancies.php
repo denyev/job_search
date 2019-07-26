6abb865690db8ababfe8dd39eb58307d91679366
denyev <denevt87@gmail.com>
2019-07-26 12:07:20 +0300

    Removes `getAll` function.

--- models/Vacancies.php
+++ models/Vacancies.php
@@ -73,34 +73,6 @@ class Vacancies extends \yii\db\ActiveRecord
     }
 
     /**
-     * Returns a full list of vacancies.
-     *
-     * @param int $pageSize - Number of vacancies per page.
-     * @return mixed
-     */
-    public static function getAll($pageSize = 1)
-    {
-        // build a DB query to get all vacancies
-        $query = Vacancies::find();
-
-        // get the total number of vacancies (but do not fetch the article data yet)
-        $count = $query->count();
-
-        // create a pagination object with the total count
-        $pagination = new Pagination(['totalCount' => $count, 'pageSize' => $pageSize]);
-
-        // limit the query using the pagination and retrieve the vacancies
-        $vacancies = $query->offset($pagination->offset)
-            ->limit($pagination->limit)
-            ->all();
-
-        $data['vacancies'] = $vacancies;
-        $data['pagination'] = $pagination;
-
-        return $data;
-    }
-
-    /**
      * @return \yii\db\ActiveQuery
      */
     public function getResponses()
